04010ed81d61befc86766d8fe33f6abac0bde370
Adan <azurerinrin@gmail.com>
2020-11-06 17:20:19 -0700

    fixed a bug on initialization

--- application/shoot_task_large.c
+++ application/shoot_task_large.c
@@ -23,11 +23,13 @@
 void shoot_task_large(void const *argument){
 	///System variables
 	 uint32_t period = osKernelSysTick();
+	
+	//PWM Initialize
+	HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_3);
 	 
 
     while(1){
 
-			
 			
 			//Idle time for the system 
 			osDelayUntil(&period,5);
